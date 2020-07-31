703aa66b121972dee2cd917d138836edb72d32bd
Josh <jwday@ucdavis.edu>
2020-07-30 19:12:33 -0700

    added minor functionality to control dpi output

--- kalman/batch_kalman_3D.py
+++ kalman/batch_kalman_3D.py
@@ -3,29 +3,41 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-from scipy import linalg
-from mpl_toolkits.mplot3d import Axes3D
-from filterpy.kalman import KalmanFilter
-import os
 import sys
 from pathlib import Path
 import seaborn as sns
 
+sns.set()
 sns.axes_style("white")
 sns.set_style("whitegrid", {"xtick.major.size": 0, "ytick.major.size": 0, 'grid.linestyle': '--'})
 sns.set_context("paper", font_scale = 1, rc={"grid.linewidth": .5})
+sns.set_palette("colorblind")
 
 directory = '~/Research Files/Videos/Discharges and Modes/A1+A2/'
+dev_run = False
 
-def batch_kalman(directory):
+def batch_kalman(directory, dev_run):
+	print('What is dev_run? It is: {}'.format(dev_run))
+
+	if dev_run == True:
+		dpi = 150
+		print('dpi is 150')
+	else:
+		dpi = 300
+		print('dpi is 300')
+
 	datafiles = [x.name for x in Path(directory).expanduser().glob('*.csv')]
+	print('Datafiles: {}'.format(datafiles))
 	all_data = pd.DataFrame()
 
-	for trial in datafiles:	
+	for i, trial in enumerate(datafiles):	
 		valvegroup = trial.split('_')[0]
 		movement_type = trial.split('_')[1]
-		trial_number = trial.split('_')[2].split('.')[0]
-		trial_name = movement_type + ' ' + trial_number
+		if dev_run == True:
+			trial_number = trial.split('_')[2].split('.')[0]
+		else:
+			trial_number = i+1
+		# trial_name = movement_type + ' ' + str(trial_number)
 
 		if valvegroup == 'Pure':
 			plot_title = 'Pure {} (No Failure Modes)'.format(movement_type)
@@ -95,19 +107,19 @@ def batch_kalman(directory):
 
 
 
-	fig5, axs = plt.subplots(3, 2, sharex=True, dpi=300, figsize=[6, 4.5])
+	fig5, axs = plt.subplots(3, 2, sharex=True, dpi=dpi, figsize=[6, 4.5])
 	fig5.suptitle(plot_title, y=0.98, fontsize=12)
 	# fig5.suptitle('Two Consecutive Plenum Discharges (No Failure Modes)', y=0.98, fontsize=12)
 
 	axs[0, 0].set_title('Total Translational Motion', fontsize=9, color='dimgrey', y=1.04)
 	axs[0, 1].set_title('Rotational Motion', fontsize=9, color='dimgrey', y=1.04)
 
-	sns.lineplot(ax=axs[0, 0], x="Time (s)", y="XY Position", hue='Trial', data=all_data, legend='full')
-	sns.lineplot(ax=axs[1, 0], x="Time (s)", y="XY Velocity", hue='Trial', data=all_data, legend='full')
-	sns.lineplot(ax=axs[2, 0], x="Time (s)", y="XY Acceleration", hue='Trial', data=all_data, legend='full')
-	sns.lineplot(ax=axs[0, 1], x="Time (s)", y="r1 Angle", hue='Trial', data=all_data, legend='full')
-	sns.lineplot(ax=axs[1, 1], x="Time (s)", y="r1 Velocity", hue='Trial', data=all_data, legend='full')
-	sns.lineplot(ax=axs[2, 1], x="Time (s)", y="r1 Acceleration", hue='Trial', data=all_data, legend='full')
+	sns.lineplot(ax=axs[0, 0], x="Time (s)", y="XY Position", palette="colorblind", hue='Trial', data=all_data, legend='full')
+	sns.lineplot(ax=axs[1, 0], x="Time (s)", y="XY Velocity", palette="colorblind", hue='Trial', data=all_data, legend='full')
+	sns.lineplot(ax=axs[2, 0], x="Time (s)", y="XY Acceleration", palette="colorblind", hue='Trial', data=all_data, legend='full')
+	sns.lineplot(ax=axs[0, 1], x="Time (s)", y="r1 Angle", palette="colorblind", hue='Trial', data=all_data, legend='full')
+	sns.lineplot(ax=axs[1, 1], x="Time (s)", y="r1 Velocity", palette="colorblind", hue='Trial', data=all_data, legend='full')
+	sns.lineplot(ax=axs[2, 1], x="Time (s)", y="r1 Acceleration", palette="colorblind", hue='Trial', data=all_data, legend='full')
 
 	axs[0, 0].set_ylabel(r'$d$ (m)', fontsize=8)
 	axs[1, 0].set_ylabel(r'$\dot{d}$ (m/s)', fontsize=8)
@@ -119,7 +131,8 @@ def batch_kalman(directory):
 
 	axs[0, 0].set_ylim(bottom=-0.015, top=0.415)		# tt
 	axs[1, 0].set_ylim(bottom=-0.01, top=0.0725)		# ttdot
-	axs[2, 0].set_ylim(bottom=-0.0425, top=0.0425)		# ttdotdot
+	# axs[2, 0].set_ylim(bottom=-0.0425, top=0.0425)		# ttdotdot
+	axs[2, 0].set_ylim(bottom=-0.0325, top=0.0525)		# ttdotdot
 	axs[0, 1].set_ylim(bottom=-360, top=360)			# r
 	axs[1, 1].set_ylim(bottom=-62, top=62)				# rdot
 	axs[2, 1].set_ylim(bottom=-30, top=30)				# rdotdot
@@ -145,7 +158,10 @@ def batch_kalman(directory):
 			ax.set(xlabel=r'Time $(sec)$')
 
 	plt.subplots_adjust(left=0.09, right=0.98, top=0.88, bottom=0.1, wspace=0.3, hspace=0.2)
-	plt.savefig(directory + 'all.png')
+	if dev_run == False:
+		plt.savefig(directory + valvegroup + '_' + movement_type + '.png')
+	else:
+		pass
 	plt.show()
 
 if __name__ == "__main__":
@@ -154,4 +170,11 @@ if __name__ == "__main__":
 	except:
 		pass  # Use defaults
 
-	batch_kalman(directory)
+	try:
+		dev_run = bool(sys.argv[2])
+	except:
+		pass
+
+	print(directory)
+	print('dev run: {}'.format(dev_run))
+	batch_kalman(directory, dev_run)
